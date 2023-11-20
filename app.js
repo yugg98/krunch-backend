0f196cdd018f3f7912352e6d795b31f2c083105b
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-20 21:40:15 +0530

    done

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+var cors = require('cors')
 
 const errorMiddleware = require("./middleware/error.js");
+app.use(cors())
+
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "backend/config/config.env" });
+}
+
 app.use(express.json());
 
 // Route Imports
-// const userRoute = require("./routes/userRoute");
+const userRoute = require("./routes/userRoute");
 
-// app.use("/api/v1", userRoute);
+app.use("/api/v1", userRoute);
 
+app.use(express.static(path.join(__dirname, "../client/out")));
 
 app.get('*',(req,res)=>{
   res.status(200).json({
@@ -16,7 +26,9 @@ app.get('*',(req,res)=>{
   })
 })
 
+app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+// Middleware for Errors
 app.use(errorMiddleware);
 
 module.exports = app;
