c216fc3845ee46189435ec8bc38ef47fdf28e56d
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-11-20 21:52:46 +0530

    done

---
+++ controllers/product.controller.js
@@ -0,0 +1,25 @@
+const productdb = require("../models/product.model");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const sendToken = require("../utils/sendToken");
+
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const myCloud = await cloudinary.v2.uploader.upload(req.body.image, {
+    folder: "productimage",
+  });
+  const { name,location,liked,category } = req.body;
+  const product = await productdb.create({
+    name,
+    location,
+    liked,
+    image: {
+      public_id: myCloud.public_id,
+      url: myCloud.secure_url,
+    },
+    category
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "Your Product is created",
+  });
+});
