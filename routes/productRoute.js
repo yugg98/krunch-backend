22365992b0e06674a610253eb0e63fecaba0054b
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-12-07 18:46:59 +0530

    done

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -8,6 +8,8 @@ router.get("/product/category", productController.getCategories);
 // Apply the isAuthenticated middleware to routes that require user validation
 router.post("/product/create", isAuthenticated, productController.createProduct);
 router.post("/product/get-by-qr", productController.getProductByQr);
+router.post("/product/create-by-qr", productController.createProductbyqr);
+
 router.put("/product/update/:id", isAuthenticated, productController.updateProduct);
 router.delete("/product/delete/:id", isAuthenticated, productController.deleteProduct);
 
