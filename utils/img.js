6a398c06962daab67e7077f2d2ea2266303099fc
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-12-08 21:34:34 +0530

    dpne

---
+++ utils/img.js
@@ -0,0 +1,7 @@
+  /*
+  https://res.cloudinary.com/yug/image/upload/v1702050956/gz3gtrfoazfzhvtmy0jc.svg
+  https://res.cloudinary.com/yug/image/upload/v1702050956/toakdmg9jhdz0garjhlh.svg
+  https://res.cloudinary.com/yug/image/upload/v1702050956/zx8zneqrpcgdhzyrljqh.svg
+  https://res.cloudinary.com/yug/image/upload/v1702050955/sa2bwnyurtov36cjlgxf.svg
+  https://res.cloudinary.com/yug/image/upload/v1702050956/xurzds4jsvnwezmxtg1m.svg
+  */
