5dd9ca895c5470ae18bff9796823017d23cf3b25
Yug Gupta <70101980+yugg98@users.noreply.github.com>
2023-12-08 21:38:55 +0530

    done

--- utils/img.js
+++ utils/img.js
@@ -1,7 +1,7 @@
   /*
-  https://res.cloudinary.com/yug/image/upload/v1702050956/gz3gtrfoazfzhvtmy0jc.svg
-  https://res.cloudinary.com/yug/image/upload/v1702050956/toakdmg9jhdz0garjhlh.svg
-  https://res.cloudinary.com/yug/image/upload/v1702050956/zx8zneqrpcgdhzyrljqh.svg
-  https://res.cloudinary.com/yug/image/upload/v1702050955/sa2bwnyurtov36cjlgxf.svg
-  https://res.cloudinary.com/yug/image/upload/v1702050956/xurzds4jsvnwezmxtg1m.svg
+  https://res.cloudinary.com/yug/image/upload/v1702050956/gz3gtrfoazfzhvtmy0jc.svg Groceries 
+  https://res.cloudinary.com/yug/image/upload/v1702050956/toakdmg9jhdz0garjhlh.svg home
+  https://res.cloudinary.com/yug/image/upload/v1702050956/zx8zneqrpcgdhzyrljqh.svg food
+  https://res.cloudinary.com/yug/image/upload/v1702050955/sa2bwnyurtov36cjlgxf.svg bever
+  https://res.cloudinary.com/yug/image/upload/v1702050956/xurzds4jsvnwezmxtg1m.svg home
   */
